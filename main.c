0fdd260690ae66e055c7cc2d5993ed8f13e7eeeb
joshtux <joshua0307lol@gmail.com>
2022-11-21 22:12:47 -0600

    21/11/2022

--- main.c
+++ main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <assert.h>
 #include <string.h>
 #include <libusb-1.0/libusb.h>
 #include <hidapi/hidapi.h>
 #define IMAGE_REPORT_LENGTH 8191
 
+
+typedef struct folder{
+	int data;
+	struct folder* left;
+	struct folder* right;
+	unsigned char image[];
+} Folder;
+typedef struct key{
+	int data;
+	int command;
+	struct folder Folder;
+	unsigned char image[];
+} Key;
+
+struct streamdeck_keys{
+	struct key Key1;
+	struct key Key2;
+	struct key Key3;
+	struct key Key4;
+	struct key Key5;
+	struct key Key6;
+};
 hid_device *streamdeck;
 int res;
 int i;
@@ -12,19 +35,13 @@ wchar_t wstr[255];
 unsigned char buf[256];
 int wait = 1;
 char key_pressed[36];
-
-struct folder{
-	int data;
-	struct folder* left;
-	struct folder* right;
-	unsigned char image[];
-};
-struct key{
-	int data;
-	int command;
-	unsigned char image[];
-};
-
+struct streamdeck_keys* root;
+struct folder* newFolder(int data)
+{}
+int command_parser( struct streamdeck_keys key)
+{
+	return 0;
+}
 int streamdeck_server()
 {
 	res = 0;
@@ -41,7 +58,22 @@ int streamdeck_server()
 			{
 				if (buf[i] == 01)
 				{
-					;
+					switch (i)
+					{
+					case 1:
+						{}
+					case 2:
+						{}
+					case 3:
+						{}
+					case 4:
+						{}
+					case 5:
+						{}
+					case 6:
+						{}
+					}
+					
 				}
 
 			}
@@ -66,6 +98,7 @@ int main()
 		hid_exit();
 		return -2;
 	}
+	struct streamdeck_keys* root;
 	streamdeck_server();
 	
 }
